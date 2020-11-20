4e396d4f773b035591b8fa9961b806f626dcd59b
Lucas Costa <62567333+Lucasmirandac@users.noreply.github.com>
2020-11-20 07:19:34 -0300

    Finalização do projeto
    commit do projeto executado.

--- main.c
+++ main.c
@@ -2,68 +2,57 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
-#include <unistd.h>
-#include <windows.h>
+#include <string.h>
 
 int velocidade_permitida (){
 
     /*Função para gerar valores inteiros aleatórios de 0 a 100*/
     int i;
     int velocidade_permitida;
-
-    for (i=0;i<10;i++){
-
-        velocidade_permitida = rand()%10*10; // Esta atribuição gera valores de 0 a 100
-
-        if (velocidade_permitida < 30) // Estamos considerando que a velocidade mínima permitida é 30km/h
-            continue;
-
-        printf("%d \n",velocidade_permitida);
-    }
+    srand(time(NULL));  //função utilizada para alterar o valor de rand
+    velocidade_permitida = 30 + rand()%10*7; // Esta atribuição gera valores de 0 a 100 com um mínimo de 30
 
     return velocidade_permitida;
 }
 
-int velocidade_atual(int velocidade_inicial){
-    /*Apartir de uma velocidade recebida aleatoriamente*/
+int velocidade_atual(){
 
-    int velocidade = velocidade_inicial;
+    /*esta função calcula o valor aleatorio da velocidade de 0 a 160*/
+
     int valor_aleatorio;
-    int contador;
 
-    while(1){
-        contador = 0;
-        srand(time(NULL));
-        valor_aleatorio = rand()%10;
-
-        if(valor_aleatorio < 5){
-
-            while(contador != 3){
-                // do something
-                velocidade = velocidade - valor_aleatorio;
-                printf("%d ", velocidade);
-                Sleep(1000);
-                contador++;
-            }
-        }
-
-        if(valor_aleatorio >= 5){
-
-            while(contador!=5){
-                velocidade = velocidade + valor_aleatorio;
-                printf("%d ", velocidade);
-                Sleep(1000);
-                contador++;
-            }
-        }
-    }
+    srand(time(NULL)); //função utilizada para alterar o valor de rand
+    valor_aleatorio = rand()%160;
+    return valor_aleatorio;
 }
 
 int main()
 {
+    struct output_cockpit{
 
-    velocidade_permitida ();
-    velocidade_atual(30);
+        int velocidade;
+        int marcador_velocidade_sugerida;
+        char situacao[15];
+    };
 
+    struct output_cockpit output;
+
+    output.marcador_velocidade_sugerida = velocidade_permitida ();
+    output.velocidade = velocidade_atual();
+
+    if (output.velocidade < output.marcador_velocidade_sugerida*0.2){
+        strcat (output.situacao,"Verde");
+        }
+    if (output.velocidade>output.marcador_velocidade_sugerida)
+        strcat (output.situacao,"Vemelho");
+
+    if (output.velocidade < output.marcador_velocidade_sugerida*0.8 && output.velocidade>=output.marcador_velocidade_sugerida*.2)
+        strcat (output.situacao,"Amarelo");
+
+    printf("Velocidade: %d \n",output.velocidade);
+    printf("Velocidade sugerida: %d \n",output.marcador_velocidade_sugerida);
+    printf ("%s \n", output.situacao);
+
+    system("pause");
     return 0;
 }
